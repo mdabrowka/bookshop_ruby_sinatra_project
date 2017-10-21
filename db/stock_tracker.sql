c5ad7da824a9d748c0e339b71bde0f20f5d4e299
Marta <martadabrowka@gmail.com>
2017-10-21 18:01:17 +0100

    set up database; set up books and authors tables

---
+++ db/stock_tracker.sql
@@ -0,0 +1,23 @@
+DROP TABLE books;
+DROP TABLE authors;
+
+
+CREATE TABLE authors (
+  id SERIAL8 PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255)
+);
+
+CREATE TABLE books (
+   id SERIAL8 PRIMARY KEY,
+   title VARCHAR(255),
+   author_id INT4 REFERENCES authors(id),
+   genre VARCHAR(255),
+   source_language VARCHAR(255),
+   buy_price INT4,
+   sell_price INT4
+  );
+
+-- INSERT INTO authors (id, first_name, last_name) VALUES (1, 'Clarice', 'Lispector');
+-- INSERT INTO books (id, title, author_id, genre, source_language, buy_price, sell_price)
+-- VALUES (1, 'Hour of a Star', 1, 'novel', 'Portuguese', 7, 14);
