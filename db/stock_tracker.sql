7d812503dca3e937d11911d527d75dc864772930
Marta <martadabrowka@gmail.com>
2017-10-24 14:03:28 +0100

    added genres table in stock_tracker.sql file

--- db/stock_tracker.sql
+++ db/stock_tracker.sql
@@ -1,4 +1,5 @@
 DROP TABLE books;
+DROP TABLE genres;
 DROP TABLE authors;
 
 
@@ -8,12 +9,17 @@ CREATE TABLE authors (
   last_name VARCHAR(255)
 );
 
+CREATE TABLE genres (
+  id SERIAL8 PRIMARY KEY,
+  type VARCHAR(255)
+);
+
 CREATE TABLE books (
    id SERIAL8 PRIMARY KEY,
    title VARCHAR(255),
    author_id INT4 REFERENCES authors(id),
    quantity INT4,
-   genre VARCHAR(255),
+   genre_id INT4 REFERENCES genres(id),
    source_language VARCHAR(255),
    buy_price INT4,
    sell_price INT4
