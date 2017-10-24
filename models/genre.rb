af62a828360e8617252c25f7208bc2d83b57ba26
Marta <martadabrowka@gmail.com>
2017-10-24 14:14:14 +0100

    added more functions to genre class

--- models/genre.rb
+++ models/genre.rb
@@ -9,3 +9,49 @@ attr_accessor :type
     @id = options['id'].to_i if options['id']
     @type = options['type']
   end
+
+
+  def save
+    sql = "INSERT INTO genres (type) VALUES ($1) RETURNING *"
+    values = [@type]
+    result = SqlRunner.run(sql, values)[0]['id']
+    @id = result.to_i
+  end
+
+  def self.all
+      sql = "SELECT * FROM genres"
+      values = []
+      result = SqlRunner.run(sql, values)
+      genres = result.map{|genre| Genre.new(genre)}
+      return genres
+    end
+
+    def self.delete_all()
+      sql = "DELETE FROM genres"
+      values = []
+      SqlRunner.run(sql, values)
+     end
+
+     def delete
+       sql = "DELETE FROM genres WHERE id = $1"
+       values=[@id]
+       SqlRunner.run(sql, values)
+     end
+
+     def update
+      sql = "UPDATE genres SET (type) =
+     ($1) WHERE id = $2"
+     values = [@type]
+     SqlRunner.run(sql, values)
+    end
+
+    def self.find(id)
+     sql = "SELECT * FROM genres WHERE id = $1"
+     values = [id]
+     results = SqlRunner.run(sql,values)[0]
+     genre = Genre.new(results)
+     return genre
+   end
+
+
+end
